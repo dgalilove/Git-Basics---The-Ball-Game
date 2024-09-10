d7e938069c5690027c390dce4c818d40886b1a56
dgalilove <dgalilove@gmail.com>
2024-09-10 21:15:46 +0300

    Update onBallClick()

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function onBallClick(elBall){
-  console.log('hello')
+function onBallClick(elBall) {
+	if (!elBall.style.height) { //because its falsy 
+		elBall.style.height = "100px"
+	}
+	if (!elBall.style.width) { //because its falsy 
+		elBall.style.width = "100px"
+	}
+
+	elBall.style.height = parseInt(elBall.style.height) + 50 + "px"
+	elBall.style.width = parseInt(elBall.style.width) + 50 + "px"
+	elBall.innerText = parseInt(elBall.style.height)
 }
